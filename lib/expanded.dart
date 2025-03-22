30fe83cffef165c2e113be438cb483bb1683e2c2
Richdadbtc <chrisuche781@gmail.com>
2025-03-22 16:45:04 +0100

    updated

--- lib/expanded.dart
+++ lib/expanded.dart
@@ -1,38 +1,42 @@
 import 'package:flutter/material.dart';
 
-
-class MyHomePage extends StatelessWidget {
+class expanded extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
+      body: Column(
         children: [
           Container(
             width: 100,
             height: 100,
             color: Colors.green,
-            margin: EdgeInsets.all(10),
+      ),
+      Expanded(
+        flex: 6,
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.amber,
+    ),
 
-          ),
-
+    ),
           Container(
             width: 100,
             height: 100,
             color: Colors.black,
-            margin: EdgeInsets.all(10),
-
           ),
-
-            Container(
+          Expanded(
+            flex: 8,
+            child: Container(
               width: 100,
               height: 100,
-              color: Colors.amber,
-              margin: EdgeInsets.all(10),
-
-
+              color: Colors.red,
+            ),
           )
-        ],
-      ),
+        ]
+      )
+
     );
+
   }
 }
